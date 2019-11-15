c99d1401f5eba0ce77ca5cec0c0faad23afc23a6
AzizAlamro <56567315+AzizAlamro@users.noreply.github.com>
2019-11-15 09:23:11 -0800

    Update Recipe.cpp

--- Recipe.cpp
+++ Recipe.cpp
@@ -4,37 +4,177 @@ CS441 - Software Engineering
 Group Project - Recipe Class
 */
 
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <string>
+#include <stdlib.h>
+#include "Drink.h"
+#include "recipe.h"
+#include "Ingredient.h"
 
-//Class
-class Recipe{
-	// Access specifier 
-    private: 
-    
-    // Data Members 
-    string recipeName; 
-    vector<Ingredient> ingredientList;
-	 
-  	public:
-  		
-    // getter and setter for Name 
-    void setRecipeName(string recipeName){
-    	this->recipeName = recipeName;
+Recipe tempRecipe;
+Recipe::Recipe()
+{}
+
+
+void Recipe::readRecipe()
+{
+	string inputLine;
+	string drink;
+	ifstream fin;
+	fin.open("File.txt");
+	if (!fin) {
+		cout << "The input file can not be opened.\n";
 	}
-	
-	string getRecipeName(){
-		return recipeName;
+	else {
+		getline(fin, inputLine);
+		if (inputLine == "Drink") {
+			getline(fin, drink);
+			while (getline(fin, inputLine)) {
+				if (inputLine != "End")
+					recipeList.push_back(inputLine);
+				else
+					break;
+			}
+		}
 	}
-	
-    //getter setter for Vector of Favorite
-    
-    void setIngredientList(vector<Ingredient> ingredientList){
-    	this->ingredientList = ingredientList;
+}
+
+void Recipe::displayRecipe()
+{
+	int length = recipeList.size();
+	for (int i = 0; i < length; i++) {
+		cout << "Step " << i + 1 << ": ";
+		cout << recipeList.at(i) << "\n";
 	}
-	
-	vector<Favorite> getIngredientList(){
-		return ingredientList;
+	cout << "\n";
+}
+
+void Recipe::readMaterials() {
+	string inputLine;
+	ifstream fin;
+	fin.open("File.txt");
+	if (!fin) {
+		cout << "The input file can not be opened.\n";
+		return;
 	}
-};
+	while (getline(fin, inputLine)) {
+		if (inputLine == "Materials")
+		{
+			while (getline(fin, inputLine))
+			{
+				if (inputLine == "End")
+					break;
+				reqMaterials.push_back(inputLine);
+			}
+		}
+	}
+}
+
+void Recipe::displayMaterials()
+{
+	int length = reqMaterials.size();
+	for (int i = 0; i < length; i++)
+	{
+		cout << "Material " << i + 1 << ": " << reqMaterials.at(i) << "\n";
+	}
+}
+
+//void Recipe::addIngredient(Ingredient newIngredient)
+//{
+//	ingredientList.push_back(newIngredient);
+//}
+
+void Recipe::displayIngredients()
+{
+	int length = ingredientList.size();
+	for (int i = 0; i < length; i++)
+	{
+		cout << "Ingredient " << i + 1 << ": " << ingredientList.at(i).getName() << "\n";
+	}
+}
+
+
+Recipe Recipe::addIngredient(Ingredient ingredient)
+{
+	ingredientList.push_back(ingredient);
+	return *this;
+}
+
+
+void Recipe::readDrinks()
+{
+
+	string inputLine;
+	string drink;
+	string ingredient;
+	type drinktype;
+	int proof;
+	price dPrice;
+	string sPrice;
+	ifstream fin;
+
+	fin.open("File.txt");
+	if (!fin) {
+		cout << "The input file can't be opened.\n";
+		return;
+	}
+
+	while (!EOF) {
+		getline(fin, inputLine);
+		if (inputLine == "Drink")
+			getline(fin, drink);
+
+		getline(fin, inputLine);
+		while (inputLine == "Ingredients")
+		{
+
+			getline(fin, ingredient);
+			getline(fin, inputLine);
+			if (inputLine == "liquid")
+				drinktype = liquid;
+			else
+				drinktype = garnish;
+			getline(fin, inputLine);
+			proof = stoi(inputLine);
+			getline(fin, inputLine);
+			sPrice = inputLine;
+			if (inputLine == "low")
+				dPrice = low;
+			else if (inputLine == "mid")
+				dPrice = mid;
+			else
+				dPrice = high;
+
+			Ingredient tempIngredient(ingredient, drinktype, proof, dPrice);
+			tempRecipe.addIngredient(tempIngredient);
+
+			getline(fin, inputLine);
+		}
+
+		while (getline(fin, inputLine))
+		{
+			if (inputLine == "Materials") {
+				while (getline(fin, inputLine)) {
+					if (inputLine == "End")
+						break;
+					tempRecipe.reqMaterials.push_back(inputLine);
+				}
+			}
+		}
+
+		getline(fin, inputLine);
+		if (inputLine == "Steps")
+		{
+			while (getline(fin, inputLine))
+			{
+				if (inputLine == "End")
+					break;
+				tempRecipe.recipeList.push_back(inputLine);
+			}
+		}
+		getline(fin, inputLine);
+		if (inputLine == "Alcohol Content")
+			getline(fin, inputLine);
+		Drink tempDrink(drink, tempRecipe, inputLine);
+
+	}
+}
